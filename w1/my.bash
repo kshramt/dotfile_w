24283060630aed1d62128ed30f9d0df6b9b9a103
kshramt <kshramt@example.com>
2017-09-01 22:56:44 +0900

    Logging tmux panes

--- w1/my.bash
+++ w1/my.bash
@@ -1,3 +1,11 @@
 export PATH="$HOME/d/p/bin:$PATH"
 
 [[ -f "$HOME"/dotfile/.Xmodmap ]] && xmodmap "$HOME"/dotfile/.Xmodmap
+
+if [[ -n "$TMUX" ]] && [[ -z "${LOGGING_TMUX:-}" ]]; then
+   export LOGGING_TMUX="$$"
+   logging_tmux_dir="$HOME/d/log/tmux"
+   mkdir -p "$logging_tmux_dir"
+   logging_tmux_base="$(date +%y%m%d%H%M%S)_$$_$(echo "$PWD" | sed -e 's|/|!|g').log"
+   tmux pipe-pane "cat > \"${logging_tmux_dir}/${logging_tmux_base}\""
+fi
