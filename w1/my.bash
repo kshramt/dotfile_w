7717f7e04a6879e889b2abe0a29d2440eba2d7ba
kshramt <kshramt@example.com>
2017-09-02 20:15:01 +0900

    Tweak

--- w1/my.bash
+++ w1/my.bash
@@ -2,10 +2,10 @@ export PATH="$HOME/d/p/bin:$PATH"
 
 [[ -f "$HOME"/dotfile/.Xmodmap ]] && xmodmap "$HOME"/dotfile/.Xmodmap
 
-if [[ -n "$TMUX" ]] && [[ -z "${LOGGING_TMUX:-}" ]]; then
-   export LOGGING_TMUX="$$"
+if [[ -n "$TMUX" ]] && [[ -z "${MY_LOGGING_TMUX:-}" ]]; then
+   export MY_LOGGING_TMUX="$$"
    logging_tmux_dir="$HOME/d/log/tmux"
    mkdir -p "$logging_tmux_dir"
-   logging_tmux_base="$(date +%y%m%d%H%M%S)_$$_$(echo "$PWD" | sed -e 's|/|!|g').log"
+   logging_tmux_base="$(date +%y%m%d%H%M%S)_$$_#I_#P_$(echo "$PWD" | sed -e 's|/|!|g').log"
    tmux pipe-pane "cat > \"${logging_tmux_dir}/${logging_tmux_base}\""
 fi
